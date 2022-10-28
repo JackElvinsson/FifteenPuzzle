370647d8359111081712fe575f906b7113c916cb
Rasmus <rasmus.adolfsson99@live.se>
2022-10-28 13:48:27 +0200

    Added actionlistener in GUI

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,13 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Main extends JFrame implements ActionListener {
+public class Main {
 
     Main () {
 
         GUI gui = new GUI();
 
     }
-
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
-
-
 
     public static void main(String[] args) {
         GameMethods gm = new GameMethods();
